c6bf502669dc5d85000075929bc88dbec0eaeca9
Timothy Hoang <timh1203@gmail.com>
2017-11-14 17:47:27 -0500

    Add pull of video data

--- script.js
+++ script.js
@@ -2,19 +2,35 @@
     const baseURL = 'https://www.googleapis.com/youtube/v3';
     const apiKey = 'AIzaSyCqCuFDGmpWKfl-9sE9wbZFnpCn-jh8Mlc';
     const fccChannel = 'UC8butISFwT-Wl7EV0hUK0BQ';
-    var callURL = `${baseURL}/activities?part=snippet,contentDetails&key=${apiKey}&channelId=${fccChannel}&maxResults=50`
+    var callURL = `${baseURL}/activities?part=snippet,contentDetails&key=${apiKey}&channelId=${fccChannel}&maxResults=5`
+    var navSearch = document.getElementById('search');
+    const channels = document.getElementById('channels');
     console.log(callURL);
     // const timChannel = 'UCUCXUCn0PEPrp7nAIFpFbOw';
     // const searchChannel = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=free+code+camp&type=video&videoCaption=closedCaption&key=AIzaSyCqCuFDGmpWKfl-9sE9wbZFnpCn-jh8Mlc&maxResults=50";
 
     function addChannels(data) {
-        const title0 = data.items[0].snippet.title;
-        const title1 = data.items[1].snippet.title;
-        const title2 = data.items[2].snippet.title;
+        const thumbnail = data.items[0].snippet.thumbnails.high.url;
+        const videoTitle = data.items[0].snippet.title;
+        const channelTitle = data.items[0].snippet.channelTitle;
+        const videoDesc = data.items[0].snippet.description;
+        const publishDate = data.items[0].snippet.publishedAt;
+        const videoID = data.items[0].contentDetails.upload.videoId;
+        const watchURL = 'https://www.youtube.com/watch?v=';
+        let content = '';
         console.log(data);
-        console.log(title0);
-        console.log(title1);
-        console.log(title2);
+        content += `
+            <div id="${videoID}">
+            <a href="${watchURL + videoID}" target="_blank"><img src="${thumbnail}" /></a>
+            <p>${videoTitle}</p>
+            <p>${channelTitle}</p>
+            <p>${videoDesc}</p>
+            <p>Published on: ${publishDate}</p>
+            <p>URL: ${watchURL + videoID}</p>
+            </div>
+        `;
+
+        channels.innerHTML = content;
     }   
 
     function getData() {
