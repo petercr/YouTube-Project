9305761c14d03678a6ec341f054b0561ced718c7
Peter <petecruckshank@gmail.com>
2017-11-23 09:05:35 -0500

    still trying to figure out why the <iframe> keeps disappearing

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 
 // ESLint file is included in GitHub project. To run ESlint run $npm install -g ESlint
 // You shold then be able to intergrate it with your Text Editor. It if is not working then install the ESLint extention
-        
 
-    
+
+
 	let videoID;
 	const baseURL = "https://www.googleapis.com/youtube/v3";
 	const apiKey = "AIzaSyCqCuFDGmpWKfl-9sE9wbZFnpCn-jh8Mlc";
 	const fccChannel = "UC8butISFwT-Wl7EV0hUK0BQ";
-	var callURL = `${baseURL}/activities?part=snippet,contentDetails&key=${apiKey}&channelId=${fccChannel}&maxResults=5`
+	var callURL = `${baseURL}/activities?part=snippet,contentDetails&key=${apiKey}&channelId=${fccChannel}&maxResults=5`;
 	var navSearch = document.getElementById("search");
 	const channels = document.getElementById("channels");
 	console.log(callURL);
@@ -17,9 +17,9 @@
 	// const searchChannel = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=free+code+camp&type=video&videoCaption=closedCaption&key=AIzaSyCqCuFDGmpWKfl-9sE9wbZFnpCn-jh8Mlc&maxResults=50";
 
 	function addChannels(data) {
-        
-        
-        
+
+
+
 		const thumbnail = data.items[0].snippet.thumbnails.high.url;
 		const videoTitle = data.items[0].snippet.title;
 		const channelTitle = data.items[0].snippet.channelTitle;
@@ -32,7 +32,6 @@
 		// player.loadVideoById(videoID);
 		content += `
             <div id="${videoID}">
-            
             <p>${videoTitle}</p>
             <p>${channelTitle}</p>
             <p>${videoDesc}</p>
@@ -43,7 +42,7 @@
 		// the line below was line 24 now replaced with an <iframe>
 		// <a href="${watchURL + videoID}" target="_blank"><img src="${thumbnail}" /></a>
 		channels.innerHTML = content;
-	}   
+	}
 
 	function getData() {
 		fetch(callURL)
@@ -51,7 +50,7 @@
 			.then(addChannels)
 			.catch(err => console.log("There was an error: ", err));
 	};
-    
+
 	getData();
 
 
